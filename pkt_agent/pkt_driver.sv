edac352e3eee671559ee7a1121cf23c20985e0e8
Ruben <rsm.ruben@gmail.com>
2022-06-12 19:16:04 +0200

    Implement driver

--- pkt_agent/pkt_driver.sv
+++ pkt_agent/pkt_driver.sv
@@ -1,4 +1,4 @@
-class pkt_driver extends uvm_driver;
+class pkt_driver extends uvm_driver #(pkt_tr);
 
     `uvm_component_utils(pkt_driver)
 
@@ -13,5 +13,35 @@ class pkt_driver extends uvm_driver;
         if (!uvm_config_db#(virtual pkt_if)::get(this, "", "pkt_vif", pkt_if))
             `uvm_fatal("NO_IF", "Couldn't retrieve pkt_if from DB")
     endfunction : build_phase
+
+    task run_phase(uvm_phase phase);
+        int half_clk_ns;
+        super.run_phase(phase);
+
+        forever begin
+            seq_item_port.get_next_item(req);
+            drive();
+            seq_item_port.item_done();
+        end
+
+    endtask : run_phase
+
+    virtual task drive();
+        case(req.freq)
+            0: half_clk_ns = 2;
+            1: half_clk_ns = 3;
+            2: half_clk_ns = 4;
+            3: half_clk_ns = 5;
+        endcase
+
+        repeat (4) begin
+            pkt_if.clk = 1'b1;
+            pkt_if.addr = req.addr[3];
+            pkt_if.data = req.data[3];
+            #(half_clk_ns * 1ns)
+            pkt_if.clk = 0;
+            #(half_clk_ns * 1ns)
+        end
+    endtask : drive
 
 endclass : pkt_driver
