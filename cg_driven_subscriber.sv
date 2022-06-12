58d629ddc9f7c998c9f49ac9f9788233e99288f7
Ruben <rsm.ruben@gmail.com>
2022-06-12 20:07:42 +0200

    Implement cg_driven_subscriber

--- cg_driven_subscriber.sv
+++ cg_driven_subscriber.sv
@@ -1,3 +1,29 @@
-class cg_driven_subscriber extends uvm_subscriber;
+import pkt_agent_pkg::pkt_tr;
+
+class cg_driven_subscriber extends uvm_subscriber #(pkt_tr);
+
+    logic [3:0] addr;
+    logic [3:0] data;
+    logic [1:0] freq;
+
+    `uvm_component_utils(cg_driven_subscriber)
+
+    covergroup c_addr;
+        coverpoint addr;
+    endgroup
+
+    c_addr cg_addr;
+
+    function new (string name = "cg_driven_subscriber", uvm_component parent);
+        super.new(name, parent);
+        cg_addr = new();
+    endfunction : new
+
+    virtual function void write (pkt_tr tr);
+        addr = tr.addr;
+        data = tr.data;
+        freq = tr.freq;
+        cg_addr.sample();
+    endfunction : write
 
 endclass : cg_driven_subscriber
