ff124145ce22b4c466facf98710f130153f8fb8f
Ruben <rsm.ruben@gmail.com>
2022-06-12 19:12:38 +0200

    Add freq property to transaction

--- pkt_agent/pkt_tr.sv
+++ pkt_agent/pkt_tr.sv
@@ -2,6 +2,7 @@ class pkt_tr extends uvm_sequence_item;
 
     rand logic [3:0] addr;
     rand logic [3:0] data;
+    rand logic [1:0] freq;
 
     function new(string name="pkt_tr");
         super.new(name);
