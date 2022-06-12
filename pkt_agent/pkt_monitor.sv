4bc0c78a3943919e6cf2ca5763d29799e0280c17
Ruben <rsm.ruben@gmail.com>
2022-06-12 19:54:03 +0200

    Implement monitor

--- pkt_agent/pkt_monitor.sv
+++ pkt_agent/pkt_monitor.sv
@@ -4,7 +4,8 @@ class pkt_monitor extends uvm_monitor;
 
     virtual pkt_if pkt_if;
 
-    uvm_analysis_port item_collected_port;
+    uvm_analysis_port#(pkt_tr) mon_analysis_port;
+    pkt_tr tr;
 
     function new(string name="pkt_monitor", uvm_component parent);
         super.new(name, parent);
@@ -15,8 +16,17 @@ class pkt_monitor extends uvm_monitor;
         if ( !uvm_config_db#(virtual pkt_if)::get(this, "", "pkt_vif", pkt_if) ) begin
             `uvm_fatal("NO_IF", "Couldn't retrieve pkt_if from UVM DB")
         end
+        mon_analysis_port = new ("mon_analysis_port", this); 
     endfunction : build_phase
 
-
+    virtual task run_phase(uvm_phase phase);
+        super.run_phase(phase);
+        tr = pkt_tr::type_id::create("tr");
+        @(posedge pkt_if.clk);
+        tr.addr = pkt_if.addr;
+        tr.data = pkt_if.data;
+        `uvm_info("MON", $sformatf("Sending transaction %0s", tr.sprint()), UVM_MEDIUM)
+        mon_analysis_port.write(tr);
+    endtask : run_phase
 
 endclass : pkt_monitor
