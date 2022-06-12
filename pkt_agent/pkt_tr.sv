2f224bd15467be65129ddab82184280e786a04f7
Ruben <rsm.ruben@gmail.com>
2022-06-12 19:29:22 +0200

    Register transaction in factory

--- pkt_agent/pkt_tr.sv
+++ pkt_agent/pkt_tr.sv
@@ -4,6 +4,12 @@ class pkt_tr extends uvm_sequence_item;
     rand logic [3:0] data;
     rand logic [1:0] freq;
 
+    `uvm_object_utils_begin(pkt_tr)
+        `uvm_field_int(addr, UVM_ALL_ON)
+        `uvm_field_int(data, UVM_ALL_ON)
+        `uvm_field_int(freq, UVM_ALL_ON)
+    `uvm_object_utils_end
+
     function new(string name="pkt_tr");
         super.new(name);
     endfunction : new
