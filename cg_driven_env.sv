66eff3a77617f5c11ab854431163d312447642a3
Ruben <rsm.ruben@gmail.com>
2022-06-12 19:01:34 +0200

    Instantiate pkt_agent in environment

--- cg_driven_env.sv
+++ cg_driven_env.sv
@@ -1,11 +1,14 @@
 class cg_driven_env extends uvm_env;
 
+    pkt_agent pkt_agt;
+
     function new(string name = "cg_driven_en", uvm_component parent);
         super.new(name, parent);
     endfunction : new
 
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
+        pkt_agt = pkt_agent::type_id::create("pkt_agt", this);
     endfunction : build_phase
 
 endclass : cg_driven_env
