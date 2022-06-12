90a8bd277eb2c2ac784cdd4378f6482298594107
Ruben <rsm.ruben@gmail.com>
2022-06-12 18:33:34 +0200

    Rename import

--- cg_driven_pkg.sv
+++ cg_driven_pkg.sv
@@ -2,7 +2,7 @@ package cg_driven_pkg;
     `include "uvm_macros.svh"
     import uvm_pkg::*;
 
-    import pkt_pkg::*;
+    import pkt_agent_pkg::*;
 
     `include "cg_driven_subscriber.sv"
     `include "cg_driven_env.sv"
