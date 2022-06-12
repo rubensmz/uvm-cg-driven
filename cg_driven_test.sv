28e1c852e32d5925a8edae9c1c754144489d7322
Ruben <rsm.ruben@gmail.com>
2022-06-12 19:00:29 +0200

    Instantiate environment in test

--- cg_driven_test.sv
+++ cg_driven_test.sv
@@ -2,12 +2,15 @@ class cg_driven_test extends uvm_test;
 
     `uvm_component_utils(cg_driven_test)
 
+    cg_driven_env env;
+
     function new(string name = "cg_driven_test", uvm_component parent);
         super.new(name, parent);
     endfunction : new
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
+        env = cg_driven_env::type_id::create("env", this);
     endfunction : build_phase
 
     virtual task main_phase(uvm_phase phase);
