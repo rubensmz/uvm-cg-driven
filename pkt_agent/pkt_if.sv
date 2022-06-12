0c85bcd69f0c165da079428796ada653f3a96da8
Ruben <rsm.ruben@gmail.com>
2022-06-12 17:43:51 +0200

    Modify data size to require less iterations

--- pkt_agent/pkt_if.sv
+++ pkt_agent/pkt_if.sv
@@ -1,4 +1,4 @@
 interface pkt_if;
     logic [3:0] addr;
-    logic [7:0] data;
+    logic [3:0] data;
 endinterface : pkt_if
--- pkt_agent/pkt_tr.sv
+++ pkt_agent/pkt_tr.sv
@@ -1,6 +1,7 @@
 class pkt_tr extends uvm_sequence_item;
 
     logic [3:0] addr;
+    logic [3:0] data;
 
     function new(string name="pkt_tr");
         super.new(name);
